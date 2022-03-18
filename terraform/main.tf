e3c2cde232aad1370aa8153159ec8fe4f3b638bb
AlbertasG <grinkevicius.albertas@gmail.com>
2022-03-18 14:58:33 +0000

    add: sandbox 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,3 +82,31 @@ module "sandbox3" {
   account_customizations_name = "sandbox"
 }
 
+module "sandbox4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+sandbox4@humn.ai"
+    AccountName               = "sandbox4-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
