ab2e5b8efcb0dc4ddd884d4fe5d0e858a3b10d1f
AlbertasG <grinkevicius.albertas@gmail.com>
2022-03-24 10:14:11 +0000

    add: initial accounts

---
+++ terraform/main.tf
@@ -0,0 +1,141 @@
+module "production" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+productionx@humn.ai"
+    AccountName               = "Production"
+    ManagedOrganizationalUnit = "Production"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "production"
+}
+
+module "non-production" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+nonproduction@humn.ai"
+    AccountName               = "Non-production"
+    ManagedOrganizationalUnit = "Non-production"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "non-production"
+}
+
+module "shared" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+sharedservices@humn.ai"
+    AccountName               = "Shared Services"
+    ManagedOrganizationalUnit = "Shared Services"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "shared-services"
+  }
+
+  account_customizations_name = "shared-services"
+}
+
+
+
+module "networking" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+networking@humn.ai"
+    AccountName               = "Networking Services"
+    ManagedOrganizationalUnit = "Shared Services"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "shared-services"
+  }
+
+  account_customizations_name = "shared-services"
+}
+
+module "security" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest+security@humn.ai"
+    AccountName               = "Security Services"
+    ManagedOrganizationalUnit = "Shared Services"
+    SSOUserEmail              = "awstest@humn.ai"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "security-services"
+  }
+
+  account_customizations_name = "security-services"
+}
